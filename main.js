e4a6ef4cdbf705e4f3b9849188ada0154c0a82df
cynthia nweke <nwekecynthia2015@gmail.com>
2023-01-08 17:04:33 +0100

    project

--- main.js
+++ main.js
@@ -2,21 +2,13 @@
 
 
 window.addEventListener("scroll", function(){
-    const nav = document.querySelector("nav");
-    nav.classList.toggle('sticky', window.scrollY > 0);
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
     });
 
 
-var pagebody=document.querySelector(".page")
-var menu=document.querySelector(".menu-container") 
-var mobilelinks=document.querySelector(".mobilelinks-container")
-
-function add(){
-    menu.classList.toggle('showbutton')
-    mobilelinks.classList.toggle('showmobilelink')
-}
-
-
+ 
+   
  const sr = ScrollReveal ({
     distance: '60px',
     duration: 2000,
