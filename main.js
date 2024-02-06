da7c27f2cfda637c2b1b4ae9d333627cd91269ec
cynthia <nwekecynthia2015@gmail.com>
2024-02-06 13:06:12 -0800

    intial commit

--- main.js
+++ main.js
@@ -16,6 +16,6 @@ window.addEventListener("scroll", function(){
 
  })
  sr.reveal('.content-main',{delay:350, origin:'left'})
- sr.reveal('.content-img,.content-service,.skill-main,.contact,.footer-container,.portfolio-item,.container-3', {delay:350, origin:'bottom'})
+ sr.reveal('.content-img,.content-service,.skill-main,.contact,.footer-container,.portfolio-item,.portfolio-item1,.container-3', {delay:350, origin:'bottom'})
 
        
